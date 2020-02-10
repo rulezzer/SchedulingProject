c71f18c144f475bf1f9d144f4234d93818f41bb4
Gennaro Caccavale <gennaro.caccavale@gmail.com>
2020-02-10 12:11:58 +0100

    Display how many rows you've chosen / something for the columns 2

--- src/com/company/SchedulingSubScene.java
+++ src/com/company/SchedulingSubScene.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,8 +12,11 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class SchedulingSubScene extends SubScene {
@@ -48,23 +54,70 @@ public class SchedulingSubScene extends SubScene {
 
         grid = new GridPane();
         Rectangle rectField;
+        Rectangle rectProcess;
 
 
         root.getChildren().add(grid);
 
+        Rectangle rectField2;
 
         for (int i = 0; i<processList.size(); i++) {
 
-            for (int j=0; j<processList.get(i).CalculateCompletion(processList, i); j++){
-                rectField = new Rectangle(50, TILE_SIZE);
+
+            this.processList.sort((o1, o2) -> o1.getArrivalTime().compareTo(o2.getArrivalTime()));
+
+            System.out.println("burst"+ processList.get(i).burst);
+
+            System.out.println("COMPLETION "+ processList.get(i).CalculateCompletion(processList, i));
+
+            // peché la prima colonna è occupata da PRocess Number
+            for (int j=1; j<processList.get(i).CalculateCompletion(processList, i); j++){
 
 
-                rectField.setStyle("-fx-fill: F96531; -fx-stroke: FFFFFF; -fx-stroke-width: 1;");
+                for (Process proc : processList){
+                    System.out.println(proc.arrivalTime + "indice  " + processList.indexOf(proc));
+//                    if (proc.arrivalTime<proc.arrivalTime+1){
+//
+//                    }
+                }
+
+                if( processList.get(i).getStartingTime(processList, i)>=processList.get(i).arrivalTime) {
+                    rectProcess = new Rectangle(TILE_SIZE, TILE_SIZE);
+
+                    rectProcess.setStyle("-fx-fill: #00AB84; "); //fai colori random
+                    rectProcess.setArcHeight(5);
+                    rectProcess.setArcWidth(5);
+                    GridPane.setConstraints(rectProcess, j, i);
+                    grid.getChildren().addAll(rectProcess);
 
 
-                GridPane.setConstraints(rectField, j, i);
-                grid.getChildren().addAll(rectField);
 
+
+                    FadeTransition ft = new FadeTransition(Duration.millis(500), rectProcess);
+                    ft.setFromValue(0.25);
+                    ft.setToValue(1);
+                    ft.play();
+
+                    TranslateTransition openNav=new TranslateTransition(Duration.millis(500), rectProcess);
+                    openNav.fromXProperty().setValue(-10);
+                    openNav.setToX(0);
+
+                    openNav.play();
+                    TranslateTransition closeNav=new TranslateTransition(new Duration(500), rectProcess);
+
+                    closeNav.setToX(-(rectProcess.getWidth()));
+
+
+                } else {
+                    rectProcess = new Rectangle(TILE_SIZE, TILE_SIZE);
+
+                    rectProcess.setStyle("-fx-fill: pink; "); //fai colori random
+                    rectProcess.setArcHeight(5);
+                    rectProcess.setArcWidth(5);
+                    GridPane.setConstraints(rectProcess, j, i);
+                    grid.getChildren().addAll(rectProcess);
+
+                }
             }
 
             StackPane stack = new StackPane();
@@ -77,12 +130,28 @@ public class SchedulingSubScene extends SubScene {
             rectField.setStyle("-fx-fill: F96231; -fx-stroke: FFFFFF; -fx-stroke-width: 1;");
 
 
+
+
             GridPane.setConstraints(stack, 0, i);
             grid.getChildren().addAll(stack);
 
 
+
         }
-        return grid;
+
+//        System.out.println("gg " + processList.size());
+//        System.out.println();
+//        System.out.println("ff " + processList.get(processList.size()).CalculateCompletion(processList, processList.size()));
+//
+//        for (int i=0; i<processList.get(processList.size()).CalculateCompletion(processList, i);i++) {
+//            Label xAxis = new Label(String.valueOf(processList.get(i).CalculateCompletion(processList, i)));
+//            GridPane.setConstraints(xAxis, i, 0);
+//
+//            grid.getChildren().add(xAxis);
+//
+//        }
+
+            return grid;
 
     }
 
